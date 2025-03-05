a1355bb48f7d92b3677c587d6efd06d0f5719db4
tuhoc <hoanganh11042kk2@gmail.com>
2025-03-05 17:42:56 +0700

    Create register new User API

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,51 @@
+import sendEmail from "../config/sendEmail";
+import UserModel from "../models/user.model";
+import bcryptjs from "bcryptjs";
+
+export async function registerUserController(request, response) {
+  try {
+    const { name, email, password } = request.body;
+
+    if (!name || !email || !password) {
+      return response.status(400).json({
+        message: "provide email, name, password",
+        error: true,
+        success: false,
+      });
+    }
+
+    const user = await UserModel.findOne({ email });
+
+    if (user) {
+      return response.json({
+        message: "Already register email",
+        error: true,
+        success: false,
+      });
+    }
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashPassword = await bcryptjs.hash(password, salt);
+
+    const payload = {
+      name,
+      email,
+      password: hashPassword,
+    };
+
+    const newUser = new UserModel(payload);
+    const save = await newUser.save();
+
+    const verityEmail = await sendEmail({
+        sendTo:email,
+        subject:"Verify email from binkeyit",
+        html: 
+    })
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}

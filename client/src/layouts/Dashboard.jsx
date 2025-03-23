9e4632425b9e5854b3cb126d1a6444d80bc48d62
tuhoc <hoanganh11042kk2@gmail.com>
2025-03-23 21:13:10 +0700

    Permission Menu

--- client/src/layouts/Dashboard.jsx
+++ client/src/layouts/Dashboard.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import UserMenu from "../components/UserMenu";
 import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Dashboard = () => {
+  const user = useSelector((state) => state.user);
+
   return (
     <div>
       <section className="bg-white">
